222dbbba5f7a421421c817e50365e5cfbcfde797
Zodiact <98137596+Zodiactz@users.noreply.github.com>
2022-05-04 16:00:53 +0700

    W1-03 Components Part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,26 +2,29 @@ import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import React, { Component } from "react";
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-                
+      <div className="app">
         <Navbar dark color="primary">
-                    
           <div className="container">
-                        
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                      
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-                  
         </Navbar>
-              
+
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
 }
-<Menu />;
 
 export default App;
